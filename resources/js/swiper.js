16bb96ced0a816a7a3ecd33319c89588b97184cd
Bernardus Billy Tjiptoning <bernardusbilly@Bernarduss-MacBook-Pro.local>
2014-11-24 02:11:48 -0800

    Revert to v.1.0

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,31 +1,33 @@
-$(window).load(function() {
-  $(function() {
-  setSwiperHeight();
+$(function(){
 
-  $(window).resize(function() {
-      setSwiperHeight();
-    });
-  
-    var mySwiper = $('.swiper-container').swiper({
-      //Your options here:
-      autoplay: 5000,
-      speed: 500,
-      mode:'horizontal',
-      loop: true,
-      grabCursor: true,
-      pagination: ".pagination",
-      paginationClickable: true,
-      createPagination: true,
-      resizeReInit: true
-      //etc..
-    });
-  })
+	setSwiperHeight();
 
-  function setSwiperHeight() {
-    $('.swiper-container').width($('.intro-section').width());
-    $('.swiper-container').height($('.intro-photo').height());
+	$(window).resize(function() {
+  		setSwiperHeight();
+  	});
+	
+  	var mySwiper = $('.swiper-container').swiper({
+    	//Your options here:
+    	autoplay: 5000,
+    	speed: 500,
+    	mode:'horizontal',
+    	loop: true,
+    	grabCursor: true,
+    	pagination: ".pagination",
+    	paginationClickable: true,
+    	createPagination: true,
+    	resizeReInit: true
+    	//etc..
+  	});
 
-    $('.swiper-slide').width($('.intro-section').width());
-    $('.swiper-slide').height($('.intro-section').height());
-  }
-});
+
+
+})
+
+function setSwiperHeight() {
+	$('.swiper-container').width($('.intro-section').width());
+	$('.swiper-container').height($('.intro-photo').height());
+
+	$('.swiper-slide').width($('.intro-section').width());
+	$('.swiper-slide').height($('.intro-section').height());
+}
